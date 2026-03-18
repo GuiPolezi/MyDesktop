4abb79677b67819cd895b34042579ca595fb1544
GuiPolezi <gui.polezi@hotmail.com>
2026-03-18 14:25:22 -0300

    Little Adjustment on Logout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,10 @@
-import { authService } from '../services/authService'
+import {Logout} from '../components/Logout'
 
 export function Home() {
   return (
     <section>
         <h1>Home (Usuário logado)</h1>
-        <button onClick={authService.signOut}>Sair</button>
+        <Logout />
     </section>
   )
 }
