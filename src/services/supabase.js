2fe4c5ff5b15aca4d1e9024450ce43fca31de396
GuiPolezi <gui.polezi@hotmail.com>
2026-04-02 08:39:18 -0300

    Refactor: Commenting the testarConexao lines on supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -4,10 +4,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY
 
-/*
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
+/*
 async function testarConexao() {
   const { data, error } = await supabase.from('usuarios').select('*');
 
